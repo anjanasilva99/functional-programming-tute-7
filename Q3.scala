c6e207db77af5fa1e32b6a79bca2eb003f2866ba
anjanasilva99 <108554413+anjanasilva99@users.noreply.github.com>
2022-09-13 08:50:18 +0530

    Update Q3.scala
    change  the answers

--- Q3.scala
+++ Q3.scala
@@ -1,39 +1,34 @@
 object Q3 extends App {
-  var acc1= new Account("20000001v",10000100,1000.00);
-  var acc2= new Account("20000002v",10000101,25000.00);
-  var acc3= new Account("20000003v",10000102,500.00);
-
-  var bank:List[Account] = List(acc1,acc2,acc3)
-
-  var display = () => bank.foreach(x=>println("["+x.acnumber+": "+x.balance+"]"))
-
-
-  println("Before trnasfer")
-  display()
-
-  //transfer 500 to acc3 from acc1
-  acc1.transfer(acc3,500)
-
-  println("\nAfter trnasfer")
-  display()
+  val x=new Accounts("1967326400v",26006784,25000.00)
+  val y=new Accounts("9904207094V",78906785,10000.00)
+  println("Before Transfering:")
+  println("NIC NO:"+x.Nic)
+  println("ACC No:"+x.accountNumber)
+  println("Balance:"+x.balance)
+  println()
+  println("NIC NO:"+y.Nic)
+  println("ACC No:"+y.accountNumber)
+  println("Balance:"+y.balance)
+  println()
+  x.transfer(y,2000.00)
+  println("After Transfering:")
+  println("NIC NO:"+x.Nic)
+  println("ACC No:"+x.accountNumber)
+  println("Balance:"+x.balance)
+  println()
+  println("NIC NO:"+y.Nic)
+  println("ACC No:"+y.accountNumber)
+  println("Balance:"+y.balance)
 }
 
-class Account(id:String, n:Int, b:Double){
+class Accounts(id:String,n:Int,b:Double){
 
-  var nic:String = id
-  var acnumber:Int = n
-  var balance:Double = b
+  val Nic:String=id
+  val accountNumber=n
+  var balance:Double=b
 
-  override def toString = "["+nic+":"+acnumber +":"+balance+"]"
-
-  def withdraw(x:Double) = if(x<=balance)this.balance = this.balance - x else println("Insufficent balance!")
-
-  def deposit(a:Double) = this.balance = this. balance + a
-
-  def transfer(account:Account, amount:Double) = {
-    this.withdraw(amount)
-    account.deposit(amount)
+  def transfer(a:Accounts,c:Double)={
+    this.balance=this.balance-c
+    a.balance=a.balance+c
   }
-
-
 }
